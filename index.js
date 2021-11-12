2cf1850b4ad22f0b145d0d76c9932df6983ab5f5
eun6304 <a01050412736@gmail.com>
2021-11-12 22:06:33 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://eun6304:dmstj112@boilerplate.i665x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    // useNewUrlParser: true, 
+    // useUnifiedTopology: true, 
+    // useCreateIndex: true, 
+    // useFindAndModify: false
+}).then(()=>console.log('MongoDB Connected..'))
+.catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
